377309480aa03a5c755b8993020f61acdbf48c5c
vijaya-sarathy <vijayasarathy.athikesavan@gmail.com>
2021-11-21 22:04:29 +0530

    P

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://vijay-graphql-server.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
